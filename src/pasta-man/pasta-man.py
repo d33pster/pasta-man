4e4bdf4a369f0bbb932785dceb8663c53db1ed19
d33pster <d33pster@icloud.com>
2024-04-15 04:47:39 +0530

    add options

--- src/pasta-man/pasta-man.py
+++ src/pasta-man/pasta-man.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+# version info
+__version__ = "1.0"
+
 # import project specific modules
 from _class import pmanager
 from encryption import Encryption
-from exceptions import NoneTypeVariable
+from exceptions import NoneTypeVariable, OptError
+from helptext import helptext
 
 # import libs
 from tkinter import *
@@ -12,6 +16,7 @@ from termcolor import colored
 from pathlib import Path
 from os.path import join as jPath, exists as there
 from os import makedirs
+from optioner import options
 import sys
 import threading
 
@@ -67,7 +72,26 @@ def checkmfile(home = str(Path.home())) -> bytes:
     return masterpassword
 
 def main():
+    # define global vars
     global encb, dencb
+    
+    # define arguments
+    shortargs = ['h', 'v']
+    longargs = ['help', 'version']
+    
+    optctrl = options(shortargs, longargs, sys.argv[1:], ifthisthennotthat=[['help', 'h'],['v', 'version']])
+    args, check, error, falseargs = optctrl._argparse()
+    
+    if not check:
+        raise OptError(error)
+    else:
+        h = helptext()
+        h.__version__ = __version__
+        if '-v' in args or '--version' in args:
+            h.showver()
+        elif '-h' in args or '--help' in args:
+            h.helper()
+    
     # -> home folder
     home = str(Path.home())
     # -> find out if .pastaman folder is there
