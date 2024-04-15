ba7da6de2aebba1bfc03e43b77d3a56442bcd751
Soumyo Deep Gupta <131422687+d33pster@users.noreply.github.com>
2024-04-16 03:17:07 +0530

    Threads Patch (#4)
    * version bump
    
    * update #Installation
    
    * update version
    
    * fixed a bug
    - a major bug with arguments was fixed by adding an error handler case for when args are provided.

--- src/pasta_man/self_launch_thread/Launcher.py
+++ src/pasta_man/self_launch_thread/Launcher.py
@@ -7,6 +7,18 @@ from os import system as run
 import sys
 
 def main():
+    # check for arguments
+    if len(sys.argv[1:])>0:
+        # convert all args into a string with white spaces between them
+        args = sys.argv[1]
+        for i in range(len(sys.argv[2:])):
+            args += " " + sys.argv[i]
+        # run 
+        run(f'pasta-man-launcher {args}')
+        # exit
+        sys.exit(0)
+    
+    # if no arguments run these
     system = os()
     if system=='Darwin' or system=='Linux':
         run('nohup pasta-man-launcher > ~/.pastaman/.log &')
