5a0d1e8abb08df7d5dffdc62a6b42809674be125
d33pster <d33pster@icloud.com>
2024-04-15 23:00:37 +0530

    fix dummy comments

--- src/pasta_man/pasta_man.py
+++ src/pasta_man/pasta_man.py
@@ -42,11 +42,6 @@ def checkmfile(home = str(Path.home())) -> bytes:
             raise NoneTypeVariable('Aborted.')
         # encode it
         masterpassword = masterpassword.encode('ascii')
-        # lock it -> store it
-        # -> define Encryption object
-        # enc = Encryption("pastaman".encode('ascii'), masterpassword)
-        # # -> lock it
-        # enc.lock()
         enct = threading.Thread(target=locker, args=(masterpassword.decode('ascii'),))
         enct.start()
         enct.join()
