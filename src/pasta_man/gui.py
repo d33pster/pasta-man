5050509ee3dcc4dcb01dadb2e84cbfd17d31845a
d33pster <d33pster@icloud.com>
2024-04-15 22:55:21 +0530

    separated all theme related functions and shenanigans into a separate file

--- src/pasta_man/gui.py
+++ src/pasta_man/gui.py
@@ -5,6 +5,7 @@
 # import project specific modules
 from pasta_man.targets import targets
 from pasta_man.encryption import Encryption
+from pasta_man.pasta_menu import __menu__
 
 # import libs
 from tkinter import *
@@ -27,132 +28,22 @@ class pmanager:
         self.parent.geometry('530x300+400+280')
         # set architecture
         self.arch = targets(masterpassword)
-        # set style -> theme
-        if not there(jPath(str(Path.home()), '.pastaman', '.defaulttheme')):
-            self.style = ttkthemes.themed_style.ThemedStyle(theme='arc')
-        else:
-            with open(jPath(str(Path.home()), '.pastaman', '.defaulttheme'), 'r') as theme:
-                defaultTheme = theme.read().replace('\n', '')
-                self.style = ttkthemes.themed_style.ThemedStyle(theme=defaultTheme)
         
         # set menu
         menu = Menu(self.parent)
         self.parent.config(menu=menu)
         # -> add menus
+        self.pasta_menu = __menu__(menuparent=menu)
         # --> add Themes menu
-        thememenu = Menu(menu)
-        menu.add_cascade(label='Theme', menu=thememenu)
-        
-        # ---> themes menu
-        if defaultTheme=='adapta':
-            thememenu.add_command(label='Adapta - Default', command=self.changeThemeToAdapta)
-        else:
-            thememenu.add_command(label='Adapta', command=self.changeThemeToAdapta)
-
-        if defaultTheme=='arc':
-            thememenu.add_command(label='Arc - Default', command=self.changeThemeToArc)
-        else:
-            thememenu.add_command(label='Arc', command=self.changeThemeToArc)
-        
-        if defaultTheme=='aquativo':
-            thememenu.add_command(label='Aquativo - Default', command=self.changeThemeToAquativo)
-        else:
-            thememenu.add_command(label='Aquativo', command=self.changeThemeToAquativo)
-        
-        if defaultTheme=='black':
-            thememenu.add_command(label='Black - Default', command=self.changeThemeToBlack)
-        else:
-            thememenu.add_command(label='Black', command=self.changeThemeToBlack)
-        
-        if defaultTheme=='blue':
-            thememenu.add_command(label='Blue - Default', command=self.changeThemeToBlue)
-        else:
-            thememenu.add_command(label='Blue', command=self.changeThemeToBlue)
-        
-        if defaultTheme=='breeze':
-            thememenu.add_command(label='Breeze - Default', command=self.changeThemeToBreeze)
-        else:
-            thememenu.add_command(label='Breeze', command=self.changeThemeToBreeze)
-        
-        if defaultTheme=='clearlooks':
-            thememenu.add_command(label='Clearlooks - Default', command=self.changeThemeToClearlooks)
-        else:
-            thememenu.add_command(label='Clearlooks', command=self.changeThemeToClearlooks)
-        
-        if defaultTheme=='elegance':
-            thememenu.add_command(label='Elegance - Default', command=self.changeThemeToElegance)
-        else:
-            thememenu.add_command(label='Elegance', command=self.changeThemeToElegance)
-        
-        if defaultTheme=='equilux':
-            thememenu.add_command(label='Equilux - Default', command=self.changeThemeToEquilux)
-        else:
-            thememenu.add_command(label='Equilux', command=self.changeThemeToEquilux)
-        
-        if defaultTheme=='keramik':
-            thememenu.add_command(label='Keramik - Default', command=self.changeThemeToKeramik)
-        else:
-            thememenu.add_command(label='Keramik', command=self.changeThemeToKeramik)
-        
-        if defaultTheme=='kroc':
-            thememenu.add_command(label='Kroc - Default', command=self.changeThemeToKroc)
-        else:
-            thememenu.add_command(label='Kroc', command=self.changeThemeToKroc)
+        # thememenu = Menu(menu)
+        # menu.add_cascade(label='Theme', menu=thememenu)
+        self.pasta_menu.Themes()
         
         # create Enclosing Frame
         self.EF = ttk.Frame(self.parent)
         self.EF.pack(fill=BOTH, expand=True)
         
         self.initthread = threading.Thread(target=self.arch.init).start()
-    
-    def savetheme(self, theme:str):
-        with open(jPath(str(Path.home()), '.pastaman', '.defaulttheme'), 'w') as themefile:
-            themefile.write(theme)
-        sys.exit(0)
-    
-    def changeThemeToKroc(self):
-        self.style.theme_use('kroc')
-        threading.Thread(target=self.savetheme, args=('kroc',)).start()
-    
-    def changeThemeToKeramik(self):
-        self.style.theme_use('keramik')
-        threading.Thread(target=self.savetheme, args=('keramik',)).start()
-    
-    def changeThemeToEquilux(self):
-        self.style.theme_use('equilux')
-        threading.Thread(target=self.savetheme, args=('equilix',)).start()
-    
-    def changeThemeToElegance(self):
-        self.style.theme_use('elegance')
-        threading.Thread(target=self.savetheme, args=('elegance',)).start()
-    
-    def changeThemeToClearlooks(self):
-        self.style.theme_use('clearlooks')
-        threading.Thread(target=self.savetheme, args=('clearlooks',)).start()
-    
-    def changeThemeToBreeze(self):
-        self.style.theme_use('breeze')
-        threading.Thread(target=self.savetheme, args=('breeze',)).start()
-    
-    def changeThemeToBlue(self):
-        self.style.theme_use('blue')
-        threading.Thread(target=self.savetheme, args=('blue',)).start()
-    
-    def changeThemeToBlack(self):
-        self.style.theme_use('black')
-        threading.Thread(target=self.savetheme, args=('black',)).start()
-    
-    def changeThemeToAquativo(self):
-        self.style.theme_use('aquativo')
-        threading.Thread(target=self.savetheme, args=('aquativo',)).start()
-    
-    def changeThemeToArc(self):
-        self.style.theme_use('arc')
-        threading.Thread(target=self.savetheme, args=('arc',)).start()
-    
-    def changeThemeToAdapta(self):
-        self.style.theme_use('adapta')
-        threading.Thread(target=self.savetheme, args=('adapta',)).start()
     
     def _makeinitscreen_(self):
         # create two notebooks
