285d53b2b5530fa236f0b1ee567bb0b4e9b8763f
d33pster <d33pster@icloud.com>
2024-04-16 12:34:02 +0530

    add bat creator

--- src/pasta_man/self_launch_thread/Launcher.py
+++ src/pasta_man/self_launch_thread/Launcher.py
@@ -17,8 +17,40 @@ def checklogfile():
             logfile.write("log file init")
 
 def makePasta():
+    batdat = """
+@REM turn printing of commands off
+@echo off
+
+@REM print status
+echo Operating System: Windows
+echo setting up pasta-man
+echo This might take a while.
+
+@REM install using pyinstaller.
+pyinstaller --onefile --noconsole pasta-man.py > NUL
+
+@REM After this delete .spec file and build folder tree.
+del build /s /q
+del pasta-man.spec
+
+@REM copy dist/pasta-man.exe to %USERPROFILE%\.pastaman\pasta-man.exe
+copy dist\pasta-man.exe %USERPROFILE%\.pastaman\pasta-man.exe
+
+@REM delete dist
+del dist /s /q
+
+@REM create vbs script
+cd %USERPROFILE%\.pastaman
+echo Set pastaShell = WScript.CreateObject("WScript.Shell") > pasta-man.vbs
+echo pastaShell.Run "%USERPROFILE%\.pastaman\pasta-man.exe", 0, False >> pasta-man.vbs
+
+@REM status
+echo Complete.
+"""
     directory = dirname(dirname(abspath(__file__))) # pasta_man directory
     chdir(directory)
+    with open(jPath(directory, 'win-setup.bat'), 'w') as batfile:
+        batfile.write(batdat)
     subprocess.Popen([f"{jPath(directory, 'win-setup.bat')}"]).wait()
 
 def main():
