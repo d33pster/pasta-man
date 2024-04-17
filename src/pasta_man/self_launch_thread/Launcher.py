8624abd3d7774f5d9f60686a2299db336b5cb3bc
d33pster <d33pster@icloud.com>
2024-04-17 21:01:06 +0530

    add docstring for launcher.py

--- src/pasta_man/self_launch_thread/Launcher.py
+++ src/pasta_man/self_launch_thread/Launcher.py
@@ -1,6 +1,45 @@
-#
-# This file is just for Entry Point Creation
-#
+"""
+Modules:
+    - internal:
+        - description: Already Comes installed with your python interpreter.
+        - contents:
+            platdorm.system (function)
+            os.system (function)
+            os.makedirs (function)
+            os.chdir (function)
+            os.path.exists (function)
+            os.path.join (function)
+            os.path.dirname (function)
+            os.path.abspath (function)
+            pathlib.Path (class)
+            sys
+            subprocess
+    - external:
+        - description: Needs to be installed using pip.
+    - Project Specific
+        - description: Modules made for this project.
+
+Hierarchy:
+    - Launcher.py
+        - contents:
+            checklogfile (function)
+            makePasta (function)
+            main (function)
+
+checklogfile:
+    - description: Check for log file, if not present, create it.
+
+makePasta:
+    - description: Creates a .bat file for windows which will be run to setup pasta-man for Windows (one-time)
+
+main:
+    - description: checks whether system is windows or others. If others, run pasta-man as an independent process, else run a vbs script in the background for windows to launch an independent pasta-man process.
+            
+Working:
+    - Check system
+    - if not windows, run pasta-man as an independent process (no setup required)
+    - if windows, run first time setup, where an exe and vbs files are created. From next time, run exe.
+"""
 
 from platform import system as os
 from os import system as run, makedirs, chdir
@@ -9,6 +48,8 @@ from pathlib import Path
 import sys, subprocess
 
 def checklogfile():
+    """## check for .log file, if not present, create it
+    """
     if not there(jPath(str(Path.home()), '.pastaman')):
         makedirs(jPath(str(Path.home()), '.pastaman'))
     
