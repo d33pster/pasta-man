51b2d302a575cbaada1ffc1a7bf55d516e3594ef
d33pster <d33pster@icloud.com>
2024-04-23 23:54:18 +0530

    updated helptext.py

--- src/pasta_man/utilities/helptext.py
+++ src/pasta_man/utilities/helptext.py
@@ -52,9 +52,15 @@ class Helptext:
         print("  |  -p or --path                     : show install path and exit.")
         print("  |  -rmc or --remove-configurations  : remove existing configs. ["+f'{f.RED}Warning{f.RESET}'+"] This is irreversible.")
         print("  |  -dwl or --doc-w-list             : list all modules of pasta-man. Enter the full-module-name for docstring.")
-        sys.exit(0)
+        print("  |  -i or --import                   : import a passwords file. Only files exported by pasta-man can be imported.")
+        print(f"  |                                     {f.RED}Syntax:{f.RESET} pasta-man --import")
+        print("  |  -e or --export                   : export passwords.")
+        print(f"  |                                     {f.RED}Syntax:{f.RESET} pasta-man --export <export-format>")
+        print("  |                                     Available export formats -> [\'csv\', \'xlsx\']")
+        print("  |  -s or --search                   : search a keyword in keyword-type.")
+        print(f"  |                                     {f.RED}Syntax:{f.RESET} pasta-man --search <keyword-type> <keyword>")
+        print("  |                                     Available keyword types -> [\'target\', \'target-type\', \'username\']")
     
     def showver(self):
         print(f'{f.BLUE}Pasta Man{f.RESET}', f'{f.RED}v{self.__version__}{f.RESET}')
         print('author: d33pster', 'GitHub:', f'{f.LIGHTBLUE_EX}https://github.com/d33pster/pasta-man{f.RESET}')
-        sys.exit(0)
